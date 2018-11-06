08dd57dcd029884c8ff9600a587525350e3cc26c
fengqi <fengqi@bigo.sg>
2018-11-06 18:22:01 +0800

    bash template arithmetic

--- bash/basic.sh
+++ bash/basic.sh
@@ -39,6 +39,36 @@ if [ $x -gt 10 ] && [ $x -lt 20 ]; then
 fi
 
 
+#-------------- arithmetic -------------------------
+# let
+let a=5+4
+let "a = 5 + 4"
+let a++
+let "a = 4 * 5"
+let "a = $1 + 30"
+
+# expr
+expr 5 + 4
+expr "5 + 4"
+expr 5+4
+expr 5 \* $1
+expr 11 % 2
+a=$( expr 10 - 3 )
+
+# Double Parentheses
+a=$(( 4 + 5 ))
+a=$((3+5))
+b=$(( a + 3 ))
+b=$(( $a + 4 ))
+echo $b # 12
+(( b++ ))
+echo $b # 13
+(( b += 3 ))
+echo $b # 16
+a=$(( 4 * 5 ))
+echo $a # 20
+
+
 #-------------- function -------------------------
 function myfunc() {
     # $1 代表第一个参数，$N 代表第 N 个参数
