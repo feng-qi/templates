5c4ec38c59319da239a16b24a264c783fb7e1a2d
Qi Feng <ffengqi@linux.ibm.com>
2019-04-29 04:36:36 -0500

    change aliases to functions

--- bash/gcc-config.sh
+++ bash/gcc-config.sh
@@ -1,15 +1,4 @@
-__GCC_SRC_DIR=$HOME/repos/gcc-git
-__RELEASE_BUILD_DIR=$HOME/builds/master
-__DEBUG_BUILD_DIR=$HOME/builds/debug
-__RELEASE_EXE_DIR=$__RELEASE_BUILD_DIR/gcc
-__DEBUG_EXE_DIR=$__DEBUG_BUILD_DIR/gcc
-
-alias xgcc="$__RELEASE_EXE_DIR/xgcc -B$__RELEASE_EXE_DIR"
-alias xg++="$__RELEASE_EXE_DIR/xg++ -B$__RELEASE_EXE_DIR"
-alias xgcc-debug="$__DEBUG_EXE_DIR/xgcc -B$__DEBUG_EXE_DIR"
-alias xg++-debug="$__DEBUG_EXE_DIR/xg++ -B$__DEBUG_EXE_DIR"
-
-alias mkrelease="make -C $__RELEASE_BUILD_DIR -j 30"
+__WARN='\033[0;31m[WARN]\033[0m'
 
 __DEBUG_FLAGS='CFLAGS="-O0 -g3 -fno-inline"             \
                CXXFLAGS="-O0 -g3 -fno-inline"           \
@@ -18,12 +7,65 @@ __DEBUG_FLAGS='CFLAGS="-O0 -g3 -fno-inline"             \
                CXXFLAGS_FOR_BUILD="-O0 -g3 -fno-inline" \
                CXXFLAGS_FOR_TARGET="-O0 -g3 -fno-inline"'
 
-alias mkdebug="make -C ${__DEBUG_BUILD_DIR}   -j 30   ${__DEBUG_FLAGS}"
+__GCC_SRC_DIR=$HOME/repos/gcc-git
+__RELEASE_BUILD_DIR=$HOME/builds/gcc-pristine
+__DEBUG_BUILD_DIR=$HOME/builds/debug
 
-alias cfggcc.="${__DEBUG_FLAGS} $__GCC_SRC_DIR/configure \
-    --enable-languages=all \
-    --with-cpu=power9      \
-    --disable-multilab     \
-    --with-long-double-128 \
-    --prefix=/tmp/gcc-tmpi \
-    --disable-bootstrap"
+alias xgcc="${__RELEASE_BUILD_DIR}/gcc/xgcc -B${__RELEASE_BUILD_DIR}/gcc"
+alias xg++="${__RELEASE_BUILD_DIR}/gcc/xg++ -B${__RELEASE_BUILD_DIR}/gcc"
+alias xgcc-debug="${__DEBUG_BUILD_DIR}/gcc/xgcc -B${__DEBUG_BUILD_DIR}/gcc"
+alias xg++-debug="${__DEBUG_BUILD_DIR}/gcc/xg++ -B${__DEBUG_BUILD_DIR}/gcc"
+
+alias mkgcc-in="make  -j 30  -C"
+
+function mkmaster() {
+    local old_branch=$(git -C ${__GCC_SRC_DIR} rev-parse --abbrev-ref HEAD)
+
+    if git -C ${__GCC_SRC_DIR} checkout master ; then
+        make -C ${__RELEASE_BUILD_DIR} -j 30 \
+            && echo -e "\nmaster build finish time: $(date '+%F %T')\n"
+    else
+        echo -e "${__WARN} git checkout master failed"
+        return 1
+    fi
+
+    echo -n "checking out old branch..."
+    if git -C ${__GCC_SRC_DIR} checkout ${old_branch} ; then
+        echo "done"
+    else
+        echo -e "\n${__WARN} git checkout old branch failed"
+        return 1
+    fi
+}
+
+function checkmaster() {
+    make -C ${__RELEASE_BUILD_DIR} -j 30 -k $@ check
+}
+
+function mkdebug() {
+    make -C ${__DEBUG_BUILD_DIR}   -j 30 $@  ${__DEBUG_FLAGS} \
+        && echo -e "\ndebug build finish time: $(date '+%F %T')"
+}
+
+function cfggcc() {
+    case "$1" in
+        debug)
+            local debug_flags=${__DEBUG_FLAGS}
+            local bootstrap="--disable-bootstrap"
+            ;;
+        release)
+            ;;
+        *)
+            echo "Usage:"
+            echo "    $0 [debug|release]"
+            return 1
+            ;;
+    esac
+
+    bash -c "${debug_flags} ${__GCC_SRC_DIR}/configure \
+        --enable-languages=all     \
+        --with-cpu=power9          \
+        --disable-multilab         \
+        --with-long-double-128     \
+        ${bootstrap} --prefix=/tmp/gcc-tmpi"
+}
