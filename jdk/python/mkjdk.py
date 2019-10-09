8e8a0913bd85bc0b6aa008ec2dbd9687d261c906
fengqi <feng.qi@msn.com>
2019-10-09 07:07:40 +0000

    add JAVA_HOME in env and --jobs option

--- jdk/python/mkjdk.py
+++ jdk/python/mkjdk.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, pprint, click
+import os, sys, pprint, click
 import subprocess as p
 from subprocess import check_call, CalledProcessError
 from pathlib import Path
@@ -10,17 +10,18 @@ def print_comand(*args, **kwargs):
 
 
 @click.command()
-@click.option('--src', '-s', default=Path('~/repos/panama').expanduser(),
+@click.option('--src', '-s', type=Path, default=Path('~/repos/panama').expanduser(),
               help='Assign OpenJDK source code directory')
-@click.option('--des', '-d', default=Path.cwd(), help='Assign build directory')
+@click.option('--des', '-d', type=Path, default=Path.cwd(), help='Assign build directory')
 @click.option('--debug_level', '-l', type=click.Choice(['fastdebug', 'slowdebug']),
               default='fastdebug', help='Set debug level')
 @click.option('--config_only', default=False, is_flag=True, help='Configure only, do not build')
 @click.option('--config_needed', '-c', default=False, is_flag=True, help='Run configure before build')
 @click.option('--target', '-t', type=click.Choice(['images', 'hotspot']), default='images',
               help='Specify build target, default is images')
+@click.option('--jobs', '-j', default=16, help='Specifies the number of jobs (commands) to run simultaneously. Default value is 16.')
 @click.option('--dry_run', default=False, is_flag=True, help='Only the show the command will be run')
-def make_jdk(src, des, debug_level, config_only, config_needed, target, dry_run):
+def make_jdk(src, des, debug_level, config_only, config_needed, target, jobs, dry_run):
 
     run = print_comand if dry_run else check_call
 
@@ -35,10 +36,11 @@ def make_jdk(src, des, debug_level, config_only, config_needed, target, dry_run)
 
     pprint.pprint(configure_cmd)
 
-    if not config_needed:
-        run(configure_cmd, cwd=des)
+    env = dict(os.environ, JAVA_HOME='/usr/lib/jvm/jdk13')
+    if config_needed:
+        run(configure_cmd, cwd=des, env=env)
     if not config_only:
-        run(['make', target], cwd=des)
+        run(['make', target, 'JOBS='+str(jobs)], cwd=des, env=env)
 
 
 if __name__ == "__main__":
