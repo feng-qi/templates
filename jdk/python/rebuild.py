dc95ef63005a2780655b939ed36d5a9b2df4c47c
fengqi <feng.qi@msn.com>
2020-01-21 04:18:38 +0000

    won't build if the target doesn't match repo branch

--- jdk/python/rebuild.py
+++ jdk/python/rebuild.py
@@ -8,10 +8,22 @@ from pathlib import Path
 def print_comand(*args, **kwargs):
     pprint.pprint(locals())
 
+
+def get_branch_in(directory):
+    cmd = ['git', 'rev-parse', '--abbrev-ref', 'HEAD']
+    return p.check_output(cmd, cwd=directory.expanduser()).decode('utf-8').strip()
+
+def dir_has_branch(directory, branch):
+    return get_branch_in(directory) == branch
+
+
 @click.command()
 @click.argument('target', type=click.Choice(['release_head', 'release_origin', 'fast_head', 'fast_origin']))
+@click.option('--repo', default=Path('~/repos/panama').expanduser(), help='Specify which repo to use')
+@click.option('--origin', '-o', default='vectorIntrinsics', help='Specify which branch is used as origin')
 @click.option('--dry_run', default=False, is_flag=True, help='Only the show the command will be run')
-def rebuild(target, dry_run):
+@click.pass_context
+def rebuild(ctx, target, repo, origin, dry_run):
 
     run = print_comand if dry_run else check_call
 
@@ -21,18 +33,15 @@ def rebuild(target, dry_run):
         fast_head      = Path('~/builds/panama/fastdebug/head'),
         fast_origin    = Path('~/builds/panama/fastdebug/origin'),
     )
-    directory = dispatch[target].expanduser()
+    build_dir = dispatch[target].expanduser()
 
-    cmd_reconfigure = ['make', 'reconfigure']
-    cmd_build = ['mkjdk.py', '--run_in', directory]
+    if 'origin' in target and not dir_has_branch(repo, origin):
+        ctx.fail(f"{target} build: repo in '{build_dir}' does not have branch '{origin}'")
 
-    try:
-        run(cmd_build)
-    except CalledProcessError:
-        print("Caught exception 'CalledProcessError', will try to reconfigure:", cmd_reconfigure)
-        run(cmd_reconfigure, cwd=directory)
-        run(cmd_build)
+    # cmd_reconfigure = ['make', 'reconfigure']
+    cmd_build = ['mkjdk.py', '--run_in', build_dir]
 
+    run(cmd_build)
 
 if __name__ == "__main__":
     rebuild()
